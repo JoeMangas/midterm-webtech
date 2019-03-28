e677a526a277fea0fa50111f57f570c39ea8c1fc
JoeMangas <mangas.j619@gmail.com>
2019-03-28 15:08:41 -0400

    added js folder, js file in js folder, jquery to catalog page

---
+++ js/catalog.js
@@ -0,0 +1,42 @@
+$("#baby")
+.on("click", function (e) {
+
+	$("#baby1").toggle();
+	$("#baby2").toggle();
+	$("#baby3").toggle();
+	$("#baby4").toggle();
+	
+	
+})
+
+$("#dogs")
+.on("click", function (e) {
+
+	$("#dogs1").toggle();
+	$("#dogs2").toggle();
+	
+	
+})
+
+$("#mens")
+.on("click", function (e) {
+
+	$("#mens1").toggle();
+	$("#mens2").toggle();
+	$("#mens3").toggle();
+	$("#mens4").toggle();
+	
+	
+})
+
+$("#womens")
+.on("click", function (e) {
+
+	$("#womens1").toggle();
+	$("#womens2").toggle();
+	$("#womens3").toggle();
+	$("#womens4").toggle();
+
+	
+	
+})
